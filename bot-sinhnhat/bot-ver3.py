bac4065feb7b350ab7dc9c52d6bcc9d009aa166f
ThanhNC <65702149+Sl33pyPanda@users.noreply.github.com>
2021-08-08 14:30:39 +0700

    fix bad requests

--- bot-sinhnhat/bot-ver3.py
+++ bot-sinhnhat/bot-ver3.py
@@ -13,30 +13,33 @@ def getToken():
         ret = re.findall('--landing-token=([a-z0-9]+)', output)
         return ret[0]        
     else:
-        raise IOError("Run lol client first plsss")
+        raise IOError("Run as Assmin or LOL client is not running ....")
         
 
 def codeEnter(code):
-    res = s.post('https://bargain.lol.garena.vn/api/enter',
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) LeagueOfLegendsClient/11.15.388.2387 (CEF 74) Safari/537.36',
-            'Token': token
-        },
-        json = {"code": code.strip(),"confirm":True}
-        #proxies = {'https': 'http://127.0.0.1:8080',
-        #            'http': 'http://127.0.0.1:8080'   }
-    )
+    for i in range(2):
+        res = s.post('https://bargain.lol.garena.vn/api/enter',
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) LeagueOfLegendsClient/11.15.388.2387 (CEF 74) Safari/537.36',
+                'Token': token,
+                'Referer': 'https://bargain.lol.garena.vn/?token=' + token
+            },
+            json = {"code": code.strip(),"confirm": True if i == 1 else False},
+            # proxies = {'https': 'http://localhost:8888'},
+            # verify = False
+        )
 
     return res.json()
 
 
 if __name__ == '__main__':
     try:
-        token = getToken()
+        #token = getToken()
+        token = "ea0445338023d1c320590226751b208fd8490ef843a4f2ae9fec3d6a24d5a8af"
         print("Token : ", token)
 
         #https://github.com/Sl33pyPanda/MiPublic/blob/master/lol-botsinhnhat/code/code
-        codes = re.findall("LOL[A-Z0-9]{10}", requests.get("https://github.com/Sl33pyPanda/MiPublic/blob/master/lol-botsinhnhat/code/code").text) 
+        codes = re.findall("LOL[A-Z0-9]{10}", requests.get("https://github.com/Sl33pyPanda/LoL-tools/blob/main/bot-sinhnhat/code/code").text) 
         print("codebase : ",len(codes))
         codes = random.sample(codes,k=1000)
         for code in codes:            
@@ -50,6 +53,10 @@ if __name__ == '__main__':
                     break
             else:
                 print(" - Failed")
+            if "error" in res:
+                if "BAD_REQUEST" in res["error"]:
+                    break
+            
 
     except Exception as e:
         print("Error  : ",end="")
